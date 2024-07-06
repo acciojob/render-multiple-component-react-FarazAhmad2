0caf5af47ab2990a75363179ec4bcd19efb3cb20
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-06 16:59:06 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <div className="ns-wrapper">
+        <h1 data-ns-test="project-name">React App</h1>
+        <h6 data-ns-test="project-description">This is my first React App</h6>
+      </div>
+    </div>
   )
 }
 
